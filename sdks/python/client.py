0c9ccdabcdac0de5b1f73e00f102083acc198444
Keagen <keegenjt@gmail.com>
2023-11-04 11:57:01 -0400

    Added simple setup

--- sdks/python/client.py
+++ sdks/python/client.py
@@ -21,15 +21,43 @@ class NetworkHandler(ss.StreamRequestHandler):
             json_data = json.loads(str(data))
             # uncomment the following line to see pretty-printed data
             # print(json.dumps(json_data, indent=4, sort_keys=True))
-            response = game.get_random_move(json_data).encode()
-            self.wfile.write(response)
-
+            response = game.analyze(json_data)
+            if response:
+                response = response.encode();
+                self.wfile.write(response)
 
 
 class Game:
+    # Notes for vars (for me)
+    init: bool;
+    info: dict;
+    units: dict;
+
     def __init__(self):
         self.units = set() # set of unique unit ids
         self.directions = ['N', 'S', 'E', 'W']
+        self.init = False;
+
+    def analyze(self, json_data):
+        # Probably not the best, way, but ehh
+        if not self.init:
+            if "game_info" in json_data:
+                Game.info = json_data['game_info'];
+                Game.init = True;
+                print(json.dumps(Game.info, indent=4, sort_keys=True))
+        else:
+            # Main logic loop
+            units = json_data['unit_updates']; 
+            tiles = json_data['tile_updates'];
+            # TODO do stuff with data
+            pass;
+
+
+
+
+    def update_map(self, json_data):
+        for tile in json_data['tile_updates']:
+            self.tile_map[tile['x']][tile['y']] = tile;
 
     def get_random_move(self, json_data):
         units = set([unit['id'] for unit in json_data['unit_updates'] if unit['type'] != 'base'])
